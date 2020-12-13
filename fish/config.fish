598e1959594b7a5aac1bdeee48b9e48d0d87d8dd
montaro <ahmed@refaey.me>
2020-12-13 04:09:18 +0100

    Create config.fish

---
+++ fish/config.fish
@@ -0,0 +1,14 @@
+# starship init fish | source
+
+set -x GOPATH /Users/d073502/workspace/sap-cx/go
+
+alias k="kubectl"
+alias g="git"
+export EDITOR='code -w'
+
+export KO_DOCKER_REPO=montaro
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/d073502/Downloads/google-cloud-sdk 3/path.fish.inc' ]; . '/Users/d073502/Downloads/google-cloud-sdk 3/path.fish.inc'; end
+set -g fish_user_paths "/usr/local/opt/python@3.8/bin" $fish_user_paths
+# set -g function __fish_describe_command; end 
