f165a57323186ade8f2ec7388aa86c936dccda13
Ahmed ElRefaey <ahmed@refaey.me>
2022-11-25 23:53:14 +0000

    oh-my-posh

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,19 @@
-# starship init fish | source
-
-set -x GOPATH /Users/montaro/workspace/go
-
-alias k="kubectl"
-alias g="git"
+alias k='kubectl'
+alias g='git'
 export EDITOR='code -w'
+export KUBE_EDITOR='code -w'
+export NVM_DIR="$HOME/.nvm"
+set -x PATH "/Library/Frameworks/Python.framework/Versions/3.10/bin" "$PATH"
+fish_add_path /opt/homebrew/bin/
+fish_add_path /opt/homebrew/opt/mysql-client/bin
 
-export KO_DOCKER_REPO=montaro
+# Spacefish
+# set SPACEFISH_DOCKER_SHOW false
+# set SPACEFISH_PROMPT_ORDER user dir host git package aws kubecontext line_sep jobs exit_code char
+# set SPACEFISH_RPROMPT_ORDER time exec_time
+# set SPACEFISH_USER_SHOW true
+# set SPACEFISH_DIR_TRUNC 5
 
-set -g fish_user_paths "/usr/local/opt/python@3.8/bin" $fish_user_paths
-# set -g function __fish_describe_command; end 
+# Oh My Posh
+oh-my-posh init fish | source
+oh-my-posh init fish --config ~/.config/fish/tokyonight_storm.omp.json | source
